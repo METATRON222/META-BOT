958b07296d3bfd4f7e938d49241ad79b30115634
METATRON222 <champion_jones22@aol.com>
2024-05-09 23:43:38 -0800

    legs

--- sq-rocket-league-starter-master/main.py
+++ sq-rocket-league-starter-master/main.py
@@ -8,4 +8,6 @@ class Bot(GoslingAgent):
     # This function runs every in-game tick (every time the game updates anything)
     def run(self):
         # set_intent tells the bot what it's trying to do
-        self.set_intent(drive(500))
+        # self.set_intent(drive(2300))
+        self.set_intent(short_shot(self.foe_goal.location))
+        print("shooting")
